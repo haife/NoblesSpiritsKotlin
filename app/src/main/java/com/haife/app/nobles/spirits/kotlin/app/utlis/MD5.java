0c8cfaeef7503990f66a1fdcd47d385b85a1317f
Haife <penghaifeng94@gmail.com>
2018-12-10 23:43:02 +0800

    Initial commit

---
+++ app/src/main/java/com/haife/app/nobles/spirits/kotlin/app/utlis/MD5.java
@@ -0,0 +1,116 @@
+package com.haife.app.nobles.spirits.kotlin.app.utlis;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * @ author haife
+ * @ email 773938795@gmail.com
+ * @ since 2017/7/19
+ * TODO：
+ */
+
+public class MD5 {
+
+    private static final char hexDigits[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+
+
+    /**
+     * MD5加密后小写输出
+     * @param str
+     * @return
+     */
+    public static String md5(String str){
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(str.getBytes());
+            byte[] b = md.digest();
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0; i < b.length; i++) {
+                int v = (int) b[i];
+                v = v < 0 ? 0x100 + v : v;
+                String cc = Integer.toHexString(v);
+                if (cc.length() == 1)
+                    sb.append('0');
+                sb.append(cc);
+            }
+            return sb.toString();
+        }
+        catch (Exception e) {
+        }
+        return "";
+    }
+
+
+    /**
+     * MD5加密
+     *
+     * @param data 明文字符串
+     * @return 16进制密文
+     */
+    public static String encryptMD5ToString(final String data) {
+        return encryptMD5ToString(data.getBytes());
+    }
+    /**
+     * MD5加密
+     *
+     * @param data 明文字符串
+     * @return 16进制加盐密文
+     */
+    public static String encryptMD5ToString(final String data, final String key) {
+        return bytes2HexString(encryptMD5((data + key).getBytes()));
+    }
+
+
+    /**
+     * MD5加密
+     *
+     * @param data 明文字节数组
+     * @return 16进制密文
+     */
+    public static String encryptMD5ToString(final byte[] data) {
+        return bytes2HexString(encryptMD5(data));
+    }
+
+
+    private static String bytes2HexString(final byte[] bytes) {
+        if (bytes == null) return null;
+        int len = bytes.length;
+        if (len <= 0) return null;
+        char[] ret = new char[len << 1];
+        for (int i = 0, j = 0; i < len; i++) {
+            ret[j++] = hexDigits[bytes[i] >>> 4 & 0x0f];
+            ret[j++] = hexDigits[bytes[i] & 0x0f];
+        }
+        return new String(ret);
+    }
+
+    /**
+     * MD5加密
+     *
+     * @param data 明文字节数组
+     * @return 密文字节数组
+     */
+    public static byte[] encryptMD5(final byte[] data) {
+        return hashTemplate(data, "MD5");
+    }
+
+    /**
+     * hash加密模板
+     *
+     * @param data      数据
+     * @param algorithm 加密算法
+     * @return 密文字节数组
+     */
+    private static byte[] hashTemplate(final byte[] data, final String algorithm) {
+        if (data == null || data.length <= 0) return null;
+        try {
+            MessageDigest md = MessageDigest.getInstance(algorithm);
+            md.update(data);
+            return md.digest();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
