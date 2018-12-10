e23c139e19d30d8a72bd34c6be620f889569e922
Haife <penghaifeng94@gmail.com>
2018-12-10 22:28:00 +0800

    Initial commit

---
+++ app/src/main/java/com/haife/app/nobles/spirits/kotlin/mvp/ui/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.haife.app.nobles.spirits.kotlin.mvp.ui.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.haife.app.nobles.spirits.kotlin.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
