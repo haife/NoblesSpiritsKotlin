7e3f843f8138ab42607a9a243b5c73ccd0d69187
Haife <penghaifeng94@gmail.com>
2019-02-16 09:07:33 +0800

    Merge branch 'master' of /Users/haife/Documents/android_space/NoblesSpiritsKotlin with conflicts.

--- app/src/main/java/com/haife/app/nobles/spirits/kotlin/mvp/ui/activity/RestaurantActivity.kt
+++ app/src/main/java/com/haife/app/nobles/spirits/kotlin/mvp/ui/activity/RestaurantActivity.kt
@@ -2,10 +2,7 @@ package com.haife.app.nobles.spirits.kotlin.mvp.ui.activity
 
 import android.content.Intent
 import android.os.Bundle
-import android.transition.TransitionInflater
-import com.alibaba.android.arouter.facade.annotation.Autowired
 import com.alibaba.android.arouter.facade.annotation.Route
-import com.alibaba.android.arouter.launcher.ARouter
 import com.haife.app.nobles.spirits.kotlin.R
 import com.haife.app.nobles.spirits.kotlin.app.base.BaseSwipeBackActivity
 import com.haife.app.nobles.spirits.kotlin.di.component.DaggerRestaurantComponent
@@ -13,8 +10,8 @@ import com.haife.app.nobles.spirits.kotlin.di.module.RestaurantModule
 import com.haife.app.nobles.spirits.kotlin.mvp.contract.RestaurantContract
 import com.haife.app.nobles.spirits.kotlin.mvp.http.router.restaurantActivityRouterUrl
 import com.haife.app.nobles.spirits.kotlin.mvp.presenter.RestaurantPresenter
-import com.haife.app.nobles.spirits.kotlin.mvp.ui.fragment.HUnionRestaurantFragment
 import com.jess.arms.di.component.AppComponent
+import me.yokeyword.fragmentation.anim.FragmentAnimator
 
 
 /**
@@ -25,10 +22,8 @@ import com.jess.arms.di.component.AppComponent
  */
 @Route(path = restaurantActivityRouterUrl)
 class RestaurantActivity : BaseSwipeBackActivity<RestaurantPresenter>(), RestaurantContract.View {
-    // 门店Id
-    @Autowired(name = HUnionRestaurantFragment.EXTRA_KEY_IMAGE_URL)
-    @JvmField
-    var merchantId: Int? = null
+    override fun post(runnable: Runnable?) {
+    }
 
     override fun setupActivityComponent(appComponent: AppComponent) {
         DaggerRestaurantComponent
@@ -41,13 +36,11 @@ class RestaurantActivity : BaseSwipeBackActivity<RestaurantPresenter>(), Restaur
 
 
     override fun initView(savedInstanceState: Bundle?): Int {
-        ARouter.getInstance().inject(this)
         return R.layout.activity_restaurant
     }
 
 
     override fun initData(savedInstanceState: Bundle?) {
-        window.enterTransition = TransitionInflater.from(this).inflateTransition(R.transition.merchant_activity_slide)
     }
 
 
@@ -67,15 +60,14 @@ class RestaurantActivity : BaseSwipeBackActivity<RestaurantPresenter>(), Restaur
 
     }
 
-    override fun onBackPressedSupport() {
-        killMyself()
-    }
-
     override fun killMyself() {
         finish()
     }
 
-    override fun post(runnable: Runnable?) {
+    override fun onCreateFragmentAnimator(): FragmentAnimator {
+        return super.onCreateFragmentAnimator()
     }
+
+
 
 }
